8888f698b43430c9e29db6695698e2c565fbe5a2
Luka <luanafernandes73@gmail.com>
2020-08-13 21:15:20 -0400

    Explicando metodos

---
+++ AulaCSharp01/Aula01/Exercicio/Entities/Funcionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaCSharp01.Aula01.Exercicio.Entities
+{
+    public class Funcionario //classe (ideia, criação, molde, definição)
+    {
+
+        public string nome; // atributos
+        public string sobrenome;
+        public string email;
+        public int idade;
+
+
+        public void MostrarEmail() // criação do método
+        {
+            Console.WriteLine("Email: " + email);
+        }
+
+        public void MostraODobroDaIdade()
+        {
+            Console.WriteLine("Dobro da idade: " + (idade * 2));
+            // + --> concatenação
+            // + --> soma
+        }
+
+        public void MostraNomeESobrenomeJuntos()
+        {
+            Console.WriteLine("Nome completo: " + nome + " " + sobrenome);
+            // Pedro
+            // Rodrigues
+            // Pedro Rodrigues
+        }
+    }
+}
---
+++ AulaCSharp01/Aula01/Exercicio/Program.cs
@@ -0,0 +1,27 @@
+﻿using AulaCSharp01.Aula01.Exercicio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaCSharp01.Aula01.Exercicio
+{
+    public class Program //Runnable
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario1 = new Funcionario(); //objeto
+            funcionario1.nome = "Pedro";
+            funcionario1.sobrenome = "Rodrigues";
+            funcionario1.email = "pedro@gmail.com";
+            funcionario1.idade = 34;
+
+            funcionario1.MostrarEmail(); //chamada do método!!
+            funcionario1.MostraNomeESobrenomeJuntos();
+            funcionario1.MostraODobroDaIdade();
+
+            Console.ReadKey();
+        }
+    }
+}
